343fe8ad595aac533f31d989fc3105048a8cb460
MINGKYME <admin@mingky.me>
2023-05-21 10:55:51 +0900

    ✨ 1.0

--- Formula/typora-uploader.rb
+++ Formula/typora-uploader.rb
@@ -1,9 +1,9 @@
 class TyporaUploader < Formula
   desc "typora image uploader"
   homepage "https://github.com/mingkyme/homebrew-typora-uploader"
-  url "https://github.com/mingkyme/homebrew-typora-uploader/releases/download/preview/typora-uploader.tar.gz"
+  url "https://github.com/mingkyme/typora-uploader/releases/download/1.0/typora-uploader-mac.zip"
   version "1.0"
-  sha256 "437ee1d95a48ad3fa08207256a7c4262b990f37ae1f68e14c1db64d3030172d6"
+  sha256 "bbe4080905de6f27a33985cd8a54bb8584b9f3a705941f643e2e813ad8c1e3cb"
   license "MIT"
 
   def install
